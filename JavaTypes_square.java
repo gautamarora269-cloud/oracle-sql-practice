5f2704a3b4cc30bf74d6b3c4e010285d1c572b1e
Gautam Arora <gautamarora269@gmail.com>
2026-01-31 10:07:50 +0530

    Create JavaTypes_square.java

---
+++ JavaTypes_square.java
@@ -0,0 +1,11 @@
+public class JavaTypes_square {
+    public static void main(String[] args) {
+
+        //Write a program to find the square of a number.
+        int number = 5;
+        int square = number + number;
+
+        System.out.println("Square of " + number + " is " + square);
+
+    }
+}
