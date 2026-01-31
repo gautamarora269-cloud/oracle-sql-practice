76a9006f3953990854c18d0cc3a2229f545457d3
Gautam Arora <gautamarora269@gmail.com>
2026-01-31 10:07:30 +0530

    Create JavaTypes_SubString.java

---
+++ JavaTypes_SubString.java
@@ -0,0 +1,9 @@
+public class JavaTypes_SubString {
+    public static void main(String[] args) {
+
+        //SubString
+        String name = "gautam and arora";
+
+        System.out.println (name.substring(0,6));
+    }
+}
